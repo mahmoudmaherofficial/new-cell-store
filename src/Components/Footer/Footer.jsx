458df487fdacda02ec14ac0288de93d4ea346000
Mahmoud Maher <mahmoudmaherofficial@gmail.com>
2024-12-28 17:57:49 +0200

    footer re-style

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="bg-dark text-white text-center py-3">
-      <p className="m-0">© 2025 Cell Store. All rights reserved.</p>
+      <p className="m-0">
+        © 2025 <strong>Cell Store</strong>. All rights reserved.
+      </p>
     </footer>
   );
-};
+}
